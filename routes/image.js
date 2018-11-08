d9d63a80fd645f63ce25b7dee71594728e228423
hafrizresa <hafrizresa@gmail.com>
2018-11-08 15:46:18 +0700

    add image upload feature

--- routes/image.js
+++ routes/image.js
@@ -1,8 +1,8 @@
 const router = require('express').Router()
 const Controller = require('../controllers/image.js')
+const Image = require('../helpers/image')
 
 router.get('/', Controller.getImages)
-router.post('/add-nael', Controller.addImageNael)
 router.get('/search?', Controller.searchImage)
 
 module.exports = router
