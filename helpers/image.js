d7ba2a3e1c3a92f2a6a1ea62439bce59df98b678
hafrizresa <hafrizresa@gmail.com>
2018-11-09 15:43:34 +0700

    add photo and image feature

--- helpers/image.js
+++ helpers/image.js
@@ -46,13 +46,13 @@ const sendUploadToGCS = (req, res, next) => {
 }
 
 const Multer = require('multer'),
-      multer = Multer({
-        storage: Multer.MemoryStorage,
-        limits: {
-          fileSize: 5 * 1024 * 1024
-        }
-        // dest: '../images'
-      })
+  multer = Multer({
+    storage: Multer.MemoryStorage,
+    limits: {
+      fileSize: 5 * 1024 * 1024
+    },
+    // dest: '../images'
+  })
 
 module.exports = {
   getPublicUrl,
--- routes/image.js
+++ routes/image.js
@@ -3,7 +3,8 @@ const Controller = require('../controllers/image.js')
 const Image = require('../helpers/image')
 
 router.get('/', Controller.getImages)
-router.post('/add',Image.multer.single('img'),Image.sendUploadToGCS, Controller.addImage)
+router.post('/add', Controller.addImage)
+router.post('/add-photo',Image.multer.single('img'), Image.sendUploadToGCS, Controller.addPhoto)
 router.post('/search?', Controller.searchImage)
 
 module.exports = router
